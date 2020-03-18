cd293b59e0291c26e8a9f0b37d3b309d61e7fa0f
Ioana Ivan <ioana.ivan@etu.univ-amu.fr>
2020-03-18 11:15:43 +0100

    Merged VAlpha version

--- Backend/src/main/java/com/mdv/services/UserService.java
+++ Backend/src/main/java/com/mdv/services/UserService.java
@@ -1,14 +1,9 @@
-/*
- * Interface exposing UserServiceImpl methods
- */
-
 package com.mdv.services;
 
 import com.mdv.model.User;
-import com.mdv.model.UserIdentifier;
 
 public interface UserService {
-
+	
 	public UserIdentifier createUser(User user);
 
 }
--- Backend/src/main/java/com/mdv/services/UserServicesImpl.java
+++ Backend/src/main/java/com/mdv/services/UserServicesImpl.java
@@ -1,40 +1,32 @@
-/*
- * Class containing services used for processing
- */
-
 package com.mdv.services;
+
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.mdv.model.*;
-import com.mdv.repository.*;
+
+import com.mdv.model.User;
+import com.mdv.repository.UserServiceJDBCTemplate;
 
 @Service
 public class UserServicesImpl implements UserService {
-
+	
 	@Autowired
-	private UserServiceJDBCTemplate userJDBC;
+	private UserServiceJDBCTemplate userJDBC ;
 
 	private static Logger log = LoggerFactory.getLogger(UserService.class);
-
+	
 	public UserIdentifier createUser(User user) {
 		log.info("User creation for user: " + user.getFirstName() + "name : " + user.getName());
-
-		// TODO repair method
-		String exist = userJDBC.findByIdCard(user.getNationalCardId());
-		// TODO test exist
-
+		
 		UserIdentifier userIdent = new UserIdentifierImpl();
-
-		// generate user identifier and password before store in DB
 		String idGen = userIdent.generateId();
 		String codeGen = userIdent.generateCode();
-		userIdent.setId(idGen);
-		userIdent.setCode(codeGen);
-
-		userJDBC.createUser(user, userIdent);
+		user.setId(idGen);
+		// TODO setCode when DB ready
+		userJDBC.createUser(user);
 		return userIdent;
 	}
 }
